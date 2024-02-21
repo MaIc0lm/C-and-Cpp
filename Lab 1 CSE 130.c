6e19d1281654d199d8fab42bb4bf6b5fdf8b63ee
Malcolm Alvarado <mjalva03@louisville.edu>
2024-02-21 13:42:40 -0500

    Lab 1 Picture
    Art using C programming Language

---
+++ Lab 1 CSE 130.c
@@ -0,0 +1,20 @@
+//Malcolm Alvarado
+//Drawing of Professor
+//CSE 130-01A Lab
+//08/22/2023
+#include "stdio.h"
+int main(int argc, char* argv[]){
+printf(   "/************\\\n"
+          "|---0----0---|\n"
+          "|      ^     |\n" 
+          "|  ######### |\n"
+          "| #\\_____/#  |\n"
+         "\\############/ \n"
+       	   "############ \n"
+           " ######### \n"
+           "  ####### \n");	
+getchar();
+return 0;
+	
+	
+	}
